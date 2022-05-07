073810933277c8d6975c6b7bbc854985264b5db6
CosmoTool <bilbao@ifca.unican.es>
2022-05-07 11:43:03 +0200

    Add files via upload

--- cipLinux.sh
+++ cipLinux.sh
@@ -1,1 +1,2 @@
-mpiifort "$1".f90 -o "$1" -O3  -I${MKLROOT}/include  ${MKLROOT}/lib/intel64/libmkl_scalapack_lp64.a -Wl,--start-group ${MKLROOT}/lib/intel64/libmkl_intel_lp64.a ${MKLROOT}/lib/intel64/libmkl_intel_thread.a ${MKLROOT}/lib/intel64/libmkl_core.a ${MKLROOT}/lib/intel64/libmkl_blacs_intelmpi_lp64.a /usr/local/lib/libcfitsio.a -Wl,--end-group -liomp5 -lpthread -lm -ldl
+mpiifort "$1".f90 -o "$1" -O3  -I"${MKLROOT}/include" -L/usr/local/lib/libcfitsio.a ${MKLROOT}/lib/intel64/libmkl_scalapack_lp64.a -Wl,--start-group ${MKLROOT}/lib/intel64/libmkl_intel_lp64.a ${MKLROOT}/lib/intel64/libmkl_intel_thread.a ${MKLROOT}/lib/intel64/libmkl_core.a ${MKLROOT}/lib/intel64/libmkl_blacs_intelmpi_lp64.a -lcfitsio -Wl,--end-group -liomp5 -lpthread -lm -ldl
+
